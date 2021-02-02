a5c78ae51a58fb23640b12d1ad408c0d6a5627a1
all0y <61382317+all0y74@users.noreply.github.com>
2021-02-02 17:16:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,15 +6,15 @@ client.once('ready', () => {
     console.log('Mudae React ON!');
 });
 
-client.login('ODAzOTkzMTcwODYxNzUyMzYw.YBF2_Q.sEVdMEBHAx0jhfrlEDMx9iAMbZM');
+client.login('TOKEN_HERE');
 
 
 client.on("message", (msg) => {
     msg.embeds.forEach((embed) => {
         var str = embed.description
         console.log(embed.description);
-        console.log(str.includes('Reaja'));
-        if (str.includes('Reaja'))
+        console.log(str.includes('React'));
+        if (str.includes('React'))
             msg.react('♥️');
         });
 });
