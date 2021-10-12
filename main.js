a1c98615d9c8dc482369f9a1566b4aba276f1d00
Gabriel <gabrielglira220@gmail.com>
2021-10-12 14:48:41 -0300

    all languages are now suported

--- main.js
+++ main.js
@@ -13,12 +13,17 @@ client.on("message", (msg) => {
     msg.embeds.forEach((embed) => {
         var str = embed.description
         console.log(embed.description);
+        if (str.includes('Reaja'))
+            msg.react('♥️');
         console.log(str.includes('Reaja'));
-        if (str.includes('Reaja'))
-        console.log(str.includes('React'));
-        if (str.includes('Reaja'))
-            msg.react('♥️');
         if (str.includes('React'))
-            msg.react('♥️');
+        msg.react('♥️');
+        console.log(str.includes('React'));
+        if (str.includes('Reaccione'))
+        msg.react('♥️');
+        console.log(str.includes('Reaccione'));
+        if (str.includes('Réagissez'))
+        msg.react('♥️');
+        console.log(str.includes('Réagissez'));
         });
 });
