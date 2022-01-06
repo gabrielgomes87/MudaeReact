e21e4759df513df63508b4e3cfd378a0d73f4958
Gabriel Gomes <61382317+all0yy@users.noreply.github.com>
2022-01-06 12:46:08 -0300

    Update main.js
    -crash fix

--- main.js
+++ main.js
@@ -13,17 +13,23 @@ client.on("message", (msg) => {
     msg.embeds.forEach((embed) => {
         var str = embed.description
         console.log(embed.description);
-        if (str.includes('Reaja'))
-            msg.react('♥️');
-        console.log(str.includes('Reaja'));
-        if (str.includes('React'))
-        msg.react('♥️');
-        console.log(str.includes('React'));
-        if (str.includes('Reaccione'))
-        msg.react('♥️');
-        console.log(str.includes('Reaccione'));
-        if (str.includes('Réagissez'))
-        msg.react('♥️');
-        console.log(str.includes('Réagissez'));
-        });
-});
+    try{
+        if (str.includes('Reaja')){
+          msg.react('♥️');
+        console.log(str.includes('Reaja'))
+        } else if (str.includes ('React')){
+          msg.react('♥️');
+        console.log(str.includes('React'))
+        } else if (str.includes('Reaccione')){
+          msg.react('♥️');
+        console.log(str.includes('Reaccione'))
+        } else if (str.includes ('Réagissez')){
+          msg.react('♥️');
+        console.log(str.includes('Réagissez'))
+        }
+    }
+    catch(e) {
+      if (e.name.toString() == "TypeError")
+        console.log("Error Null")
+    }
+})});
